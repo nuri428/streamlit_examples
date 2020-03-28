14cd1484d4c1be35a315c71b4ce103a12bc8033b
nuri <greennuri@gmail.com>
2020-03-28 15:53:20 +0900

    streamlit example1 작성

---
+++ README.md
@@ -0,0 +1,24 @@
+# STREAMLIT Examples 
+## example1.py 
+- streamlist에서 메뉴 간에 데이터 공유 예제 
+- 실행 시키면 첫번째 메뉴에서 사이드바가 표시되고 0~10 까지 값을 선택 한후 메뉴2를 선택하면 메뉴1에서 선택된 값이 메뉴2에 표시 됨. 
+- 메뉴2에서 나온 사이드바에서 값을 바꾸면 똑같이 메뉴 1에 적용됨 
+
+<img src=./imgs/ex1-1.png>
+<img src=./imgs/ex1-2.png>
+메뉴 1,2에서 사이드바 기본값이 0 
+
+<img src=./imgs/ex1-3.png>
+메뉴 1에서 사이드값을 바꿈
+
+<img src=./imgs/ex1-4.png>
+메뉴 2에서 메뉴1의 값이 반영됨 
+
+<img src=./imgs/ex1-5.png>
+메뉴 2에서 값을 바꿈 
+
+<img src=./imgs/ex1-6.png>
+메뉴 1에 반영됨 
+
+
+
---
+++ examples/example1.py
@@ -0,0 +1,60 @@
+import streamlit as st
+
+
+# for local cache
+class LocalCache():
+    def __init__(self):
+        self.user_value =0
+    def init_value(self, value):
+        self.user_value = 0 
+
+    def set_value(self, value):
+        self.user_value = value 
+
+    def get_value(self):        
+        return self.user_value
+
+
+#for cache 객체 
+# 이 함수는 LocalCache를 캐싱 처리 함 
+@st.cache(allow_output_mutation=True)
+def get_instance():
+    ins = LocalCache()
+    return ins    
+
+def test_main1():    
+    min_val = 0 
+    max_val = 10 
+    gap = 1 
+    old_val = get_instance().get_value()
+
+    st.write("screen 1")
+    val = st.sidebar.slider("select value", min_val, max_val,value=old_val, step=gap)
+    get_instance().set_value(val)
+    st.write(f"you select {get_instance().get_value()}")
+
+
+def test_main2():
+    val = get_instance().get_value()
+    st.write("screen 2")
+
+    st.write(f"you select {val}")
+    min_val = 0 
+    max_val = 10 
+    gap = 1 
+
+    val2 = st.sidebar.slider("select value", min_val, max_val, val, gap)
+    get_instance().set_value(val2)
+    st.write(f"you select {get_instance().get_value()}")
+
+
+def main():
+    choose_menu = st.selectbox("choose menu", (["menu1","menu2"]))
+    if choose_menu == "menu1":
+        test_main1()
+    elif choose_menu == "menu2":
+        test_main2()
+
+
+if __name__ == "__main__":
+    main()
